341845cbf18c8b86dda18037e9666dcc799edaab
Young <iamlovedit@163.com>
2024-02-16 10:36:12 +0800

    add new modules

---
+++ src/Prometheus.Modules.Search/SearchModule.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Prometheus.Core;
+using Prometheus.Modules.Search.Views;
+
+namespace Prometheus.Modules.Search
+{
+    public class SearchModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<SearchView>(RegionNames.SearchView);
+        }
+    }
+}
---
+++ src/Prometheus.Modules.Search/Views/SearchView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Prometheus.Modules.Search.Views
+{
+    /// <summary>
+    /// Interaction logic for SearchView
+    /// </summary>
+    public partial class SearchView : UserControl
+    {
+        public SearchView()
+        {
+            InitializeComponent();
+        }
+    }
+}
