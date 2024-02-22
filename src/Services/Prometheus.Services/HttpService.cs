a60aece3764b979ffd0db3e28e22d7cfd973bfa5
Young <iamlovedit@163.com>
2024-02-22 11:02:36 +0800

    implement and enrich http service

--- src/Services/Prometheus.Services/HttpService.cs
+++ src/Services/Prometheus.Services/HttpService.cs
@@ -1,7 +1,10 @@
 ﻿using Prometheus.Services.Interfaces;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +12,23 @@ namespace Prometheus.Services
 {
     public class HttpService : IHttpService
     {
+        private readonly string _jsonType = "application/json";
+
         private HttpClient _httpClient;
 
-        public Task<string> GetAsync()
+        public async Task<string> GetAsync(string url, IEnumerable<string> queryParameters)
         {
-            throw new NotImplementedException();
+            var targetUrl = queryParameters == null ? url : BuildQueryString(queryParameters);
+            return await _httpClient.GetStringAsync(targetUrl);
+        }
+
+        public async Task<byte[]> GetByteArrayResponseAsync(HttpMethod httpMethod, string url, IEnumerable<string> queryParameters = null)
+        {
+            var targetUrl = queryParameters == null ? url : BuildQueryString(queryParameters);
+            var requestMessage = new HttpRequestMessage(httpMethod, targetUrl);
+            var response = await _httpClient.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsByteArrayAsync();
         }
 
         public void Initialize(int port, string token)
@@ -31,10 +46,35 @@ namespace Prometheus.Services
                 DefaultRequestVersion = new Version(2, 0),
                 Timeout = TimeSpan.FromSeconds(10)
             };
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_jsonType));
             _httpClient.DefaultRequestHeaders.Add("User-Agent", "LeagueOfLegendsClient/12.7.433.4138 (CEF 91)");
             _httpClient.DefaultRequestHeaders.Connection.Add("keep-alive");
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(tokenBytes));
+        }
+
+        public async Task<string> PostAsync(string url, object body, IEnumerable<string> queryParameters)
+        {
+            var targetUrl = queryParameters == null ? url : BuildQueryString(queryParameters);
+            var responseMessage = await _httpClient.PostAsJsonAsync(targetUrl, body);
+            responseMessage.EnsureSuccessStatusCode();
+            return await responseMessage.Content.ReadAsStringAsync();
+        }
+
+        private static string BuildQueryString(IEnumerable<string> parameters)
+        {
+            return "?" + string.Join("&", parameters.Where(s => !string.IsNullOrWhiteSpace(s)));
+        }
+
+        public async Task<string> SendAsync(HttpMethod httpMethod, string url, object body, IEnumerable<string> queryParameters)
+        {
+            var targetUrl = queryParameters == null ? url : BuildQueryString(queryParameters);
+            var requestMessage = new HttpRequestMessage(httpMethod, targetUrl)
+            {
+                Content = new StringContent(body?.ToString(), Encoding.UTF8, _jsonType)
+            };
+            var response = await _httpClient.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
