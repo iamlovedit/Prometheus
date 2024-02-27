fed3cbd2ddca3b94aaf6297103bf44a2cec93aff
Young <iamlovedit@163.com>
2024-02-27 17:03:56 +0800

    add new controls

---
+++ src/Prometheus.Modules.Summoner/Views/SelectBackgroundDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Prometheus.Modules.Summoner.Views
+{
+    /// <summary>
+    /// Interaction logic for SelectBackgroundDialog
+    /// </summary>
+    public partial class SelectBackgroundDialog : UserControl
+    {
+        public SelectBackgroundDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Prometheus.Modules.Summoner/Views/TierControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Prometheus.Modules.Summoner.Views
+{
+    /// <summary>
+    /// TierControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class TierControl : UserControl
+    {
+        public TierControl()
+        {
+            InitializeComponent();
+        }
+
+        public string TierImage
+        {
+            get { return (string)GetValue(TierImageProperty); }
+            set { SetValue(TierImageProperty, value); }
+        }
+        public static readonly DependencyProperty TierImageProperty =
+            DependencyProperty.Register("TierImage", typeof(string), typeof(TierControl), new PropertyMetadata());
+
+
+
+
+        public string Tier
+        {
+            get { return (string)GetValue(TierProperty); }
+            set { SetValue(TierProperty, value); }
+        }
+
+        public static readonly DependencyProperty TierProperty =
+            DependencyProperty.Register("Tier", typeof(string), typeof(TierControl), new PropertyMetadata());
+
+
+
+        public string TierType
+        {
+            get { return (string)GetValue(TierTypeProperty); }
+            set { SetValue(TierTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TierTypeProperty =
+            DependencyProperty.Register("TierType", typeof(string), typeof(TierControl), new PropertyMetadata());
+
+    }
+}
