bc305bd04eb74a65138604044cf2f2323157357b
Young <iamlovedit@163.com>
2024-03-05 17:56:05 +0800

    add new model

---
+++ src/Prometheus.Core/Models/ChampionMastery.cs
@@ -0,0 +1,31 @@
+﻿namespace Prometheus.Core.Models
+{
+    public class ChampionMastery
+    {
+        public int ChampionId { get; set; }
+
+        public int ChampionLevel { get; set; }
+
+        public int ChampionPoints { get; set; }
+
+        public int ChampionPointsSinceLastLevel { get; set; }
+
+        public int ChampionPointsUntilNextLevel { get; set; }
+
+        public bool ChestGranted { get; set; }
+
+        public string FormattedChampionPoints { get; set; }
+
+        public string FormattedMasteryGoal { get; set; }
+
+        public string HighestGrade { get; set; }
+
+        public long LastPlayTime { get; set; }
+
+        public int PlayerId { get; set; }
+
+        public string Puuid { get; set; }
+
+        public int TokensEarned { get; set; }
+    }
+}
