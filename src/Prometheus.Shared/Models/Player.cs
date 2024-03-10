eb6907d69ca481b067737e3fbd7b6120bcf96984
Young <iamlovedit@163.com>
2024-03-10 20:14:45 +0800

    added models

---
+++ src/Prometheus.Shared/Models/Player.cs
@@ -0,0 +1,45 @@
+﻿namespace Prometheus.Shared.Models
+{
+    public class Player
+    {
+        public string ChampionIcon { get; set; }
+
+        public bool Win { get; set; }
+
+        public uint Id { get; set; }
+
+        public string Name { get; set; }
+
+        public uint PerkId { get; set; }
+
+        public uint Spell1Id { get; set; }
+
+        public uint Spell2Id { get; set; }
+
+        public uint Assists { get; set; }
+
+        public byte ChampLevel { get; set; }
+
+        public uint Deaths { get; set; }
+
+        public uint Kills { get; set; }
+
+        public uint GoldEarned { get; set; }
+
+        public uint Item0 { get; set; }
+
+        public uint Item1 { get; set; }
+
+        public uint Item2 { get; set; }
+
+        public uint Item3 { get; set; }
+
+        public uint Item4 { get; set; }
+
+        public uint Item5 { get; set; }
+
+        public uint Item6 { get; set; }
+
+        public ulong TotalDamage { get; set; }
+    }
+}
---
+++ src/Prometheus.Shared/Models/Team.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Prometheus.Shared.Models
+{
+    public class Team
+    {
+        public bool Win
+        {
+            get
+            {
+                return Players?.FirstOrDefault()?.Win ?? false;
+            }
+        }
+
+        private string _kda;
+        public string KDA
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_kda))
+                {
+                    _kda = $"{Players.Sum(p => p.Kills)}/{Players.Sum(p => p.Deaths)}/{Players.Sum(p => p.Assists)}";
+                }
+                return _kda;
+            }
+
+        }
+
+        public uint Gold => (uint)Players.Sum(p => p.GoldEarned);
+
+        public List<Player> Players { get; set; }
+    }
+}
